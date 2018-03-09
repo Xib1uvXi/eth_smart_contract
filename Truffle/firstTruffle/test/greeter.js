13c4362a80e7eff2044c9bd89a0a7e72ffccfee5
wangxi <wangxi@qiyuanfin.com>
2018-03-09 18:38:50 +0800

    add

--- Truffle/firstTruffle/test/greeter.js
+++ Truffle/firstTruffle/test/greeter.js
@@ -1,11 +1,25 @@
 const Greeter = artifacts.require("Greeter.sol");
 
 contract('Greeter.sol', async (accounts) => {
-    it('should greet() return xib', async () => {
+    it('test greet() return xib', async () => {
         let instance = await Greeter.deployed();
 
         let greetResult = await instance.greet.call();
 
         assert.equal(greetResult, "xib")
     });
+
+    it('test setGreeting()', async () => {
+        let instance = await Greeter.deployed();
+
+        let setGreetResult = await instance.setGreeting.call("gigi");
+
+        console.log(setGreetResult);
+
+        let greetResult = await instance.greet.call();
+
+        assert.equal(greetResult, "gigi")
+    });
+
+
 })
